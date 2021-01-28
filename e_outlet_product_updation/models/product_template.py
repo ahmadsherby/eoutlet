74c806bc918e63ea6b52f1fec0aa7284b06be986
magdii <magdysala@gmail.com>
2021-01-28 13:43:13 +0300

    e-outlet modifications

--- e_outlet_product_updation/models/product_template.py
+++ e_outlet_product_updation/models/product_template.py
@@ -3,6 +3,16 @@
 from odoo import models, fields, api, _
 
 
+class ProductZone(models.Model):
+    _name = 'product.zone'
+    name = fields.Char()
+
+
+class ProductShelf(models.Model):
+    _name = 'product.shelf'
+    name = fields.Char()
+
+
 class ProductVendor(models.Model):
     _name = 'product.vendor'
     name = fields.Char()
@@ -48,8 +58,31 @@ class ProductSize(models.Model):
     name = fields.Char()
 
 
+# class ProductProduct(models.Model):
+#     _inherit = 'product.product'
+
+    # @api.onchange('product_tmpl_id')
+    # def onchange_product_template_id(self):
+    #     print(self.product_tmpl_id.name)
+    #     print(self.product_tmpl_id.barcode)
+    #     print(self.product_tmpl_id.default_code)
+
+    # @api.onchange
+    # def write(self, default_code):
+    #     res = super(self.product_tmpl_id).write(default_code)
+    #     print(" work create")
+    #     return res
+
+    # # @api.model
+    #  def write(self, values):
+    #      res = super(TestStudent, self).write(values)
+    #      print("ooooo")
+    #      return res
+
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+    available_in_pos = fields.Boolean(default=True)
 
     product_vendor_id = fields.Many2one('product.vendor', string="Vendor")
     product_brand_id = fields.Many2one('product.brand', string="Brand")
@@ -60,3 +93,11 @@ class ProductTemplate(models.Model):
     product_country_id = fields.Many2one('product.country', string="country")
     product_color_id = fields.Many2one('product.color', string="color")
     product_size_id = fields.Many2one('product.size', string="size")
+    product_zone_id = fields.Many2one('product.zone', string="Product Zone")
+    product_shelf_id = fields.Many2one('product.shelf', string="Product Shelf")
+
+    # @api.onchange('product_tmpl_id')
+    # def onchange_product_template_id(self):
+    #     print(self.product_tmpl_id.name)
+    #     print(self.product_tmpl_id.barcode)
+    #     print(self.product_tmpl_id.default_code)
