aecfd612271a9fe2bc1227cf4e9fe3db1caa6354
magdii <magdysala@gmail.com>
2021-03-29 15:14:37 +0300

    e-outlet modifications

--- e_outlet_product_updation/models/product_template.py
+++ e_outlet_product_updation/models/product_template.py
@@ -105,65 +105,6 @@ class ProductProduct(models.Model):
             product_obj[i].barcode_new_name = barcode
 
 
-#             for product in product_obj:
-#                  _logger.info(red + "now ::::::::::::::: %s" % datetime.now() + reset)
-#                 product.barcode_new_name = product.barcode
-#                 product.barcode = randint(1300000000000, 8000000000000)
-
-# @api.model
-# def default_get(self, fields_list):
-#     """get default lines"""
-#     print("LLLLLLLLLLLLLLLLLL", fields_list)
-#     # print("LLLLLLLLLLLLLLLLLL", fields_list['default_code'])
-#     print("LLLLLLLLLLLLLLLLLL", self.default_code)
-#     res = super(ProductProduct, self).default_get(fields_list)
-#     res['default_code'] = str(self.product_tmpl_id.default_code) + '999999999999999999'
-#     return res
-
-# @api.model
-# def create(self, vals_list):
-#     products = super(ProductProduct, self).create(vals_list)
-#     print(":::::::::::::ProductProduct create:::::::::::::::", products)
-#     return products
-#
-# def write(self, values):
-#     print(":::::::::::::ProductProduct values:::::::::::::::", values)
-#     print(":::::::::::::product_tmpl_id:::::::::::::::", self.product_tmpl_id)
-#     print(":::::::::::::default_code:::::::::::::::", self.product_tmpl_id.default_code)
-#     res = super(ProductProduct, self).write(values)
-#     print(":::::::::::::default_codeeeeeeeeee:::::::::::::::", self.product_tmpl_id.default_code)
-#     if 'active' in values:
-#         if self.product_tmpl_id:
-#            if self.product_tmpl_id.default_code:
-#                 # print("product_template_attribute_value_ids", self.product_template_attribute_value_ids)
-#                 self.default_code = str(self.product_tmpl_id.default_code)
-#            # if self.product_tmpl_id.barcode:
-#            #      # print("product_template_attribute_value_ids", self.product_template_attribute_value_ids)
-#            #      self.barcode = str(self.product_tmpl_id.barcode)
-#                                 # + str(self.product_template_attribute_value_ids[0]['name'])
-#     # print(":::::::::::::ProductProduct res write:::::::::::::::", res)
-#     # raise Warning(_("<<<<<<<<<<<<<<<<<<<"))
-#     return res
-
-# @api.onchange('product_tmpl_id')
-# def onchange_product_template_id(self):
-#     print(">>>>>>>>>>>>>>>>>>>::::::::::::::::::", self.product_tmpl_id.name)
-#     print(">>>>>>>>>>>>>>>>>>>::::::::::::::::::", self.product_tmpl_id.barcode)
-#     print(">>>>>>>>>>>>>>>>>>>::::::::::::::::::", self.product_tmpl_id.default_code)
-
-# @api.onchange
-# def write(self, default_code):
-#     res = super(self.product_tmpl_id).write(default_code)
-#     print(" work create")
-#     return res
-
-# # @api.model
-#  def write(self, values):
-#      res = super(TestStudent, self).write(values)
-#      print("ooooo")
-#      return res
-
-
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     available_in_pos = fields.Boolean(string='Available in POS',
@@ -172,9 +113,7 @@ class ProductTemplate(models.Model):
 
     type = fields.Selection(default='product')
     price_before_discount = fields.Float()
-
     barcode_new_name = fields.Char(string="Barcode")
-
     product_vendor_id = fields.Many2one('product.vendor', string="Vendor")
     product_brand_id = fields.Many2one('product.brand', string="Brand")
     product_department_id = fields.Many2one('product.department', string="Department")
@@ -215,22 +154,30 @@ class ProductTemplate(models.Model):
                 })
         return products
 
-    # def write(self, values):
-    #     print("KKKKKKKKKKKKKKKKKKKKK", values)
-    #     res = super(ProductTemplate, self).write(values)
-    #     product_obj = self.env['product.product'].search([('product_tmpl_id.id', '=', self.id)])
-    #     barcode = self.barcode
-    #     default_code = self.default_code
-    #     for p in product_obj:
-    #         if barcode and not p.barcode:
-    #             if p.product_template_attribute_value_ids:
-    #                 print("p.product_template_attribute_value_ids", p.product_template_attribute_value_ids)
-    #                 p.update({
-    #                     'barcode': str(barcode) + '-' + str(p.product_template_attribute_value_ids.name)
-    #                 })
-    #         if default_code and not p.default_code:
-    #             p.update({
-    #                 'default_code': str(default_code)
-    #             })
-    #
-    #     return res
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    product_vendor_id = fields.Many2one('product.vendor', strore=True, force_save=True,
+                                        string="Vendor", related='product_id.product_vendor_id')
+    product_brand_id = fields.Many2one('product.brand', strore=True, force_save=True,
+                                       string="Brand", related='product_id.product_brand_id')
+    product_department_id = fields.Many2one('product.department', strore=True, force_save=True,
+                                            string="Department",
+                                            related='product_id.product_department_id')
+    product_season_id = fields.Many2one('product.season', strore=True, force_save=True,
+                                        string="season", related='product_id.product_season_id')
+    product_model_id = fields.Many2one('product.modell', strore=True, force_save=True,
+                                       string="product model", related='product_id.product_model_id')
+    product_categori_id = fields.Many2one('product.categori', strore=True, force_save=True,
+                                          string="category", related='product_id.product_categori_id')
+    product_country_id = fields.Many2one('product.country', strore=True, force_save=True,
+                                         string="country", related='product_id.product_country_id')
+    product_color_id = fields.Many2one('product.color', strore=True, force_save=True,
+                                       string="color", related='product_id.product_color_id')
+    product_size_id = fields.Many2one('product.size', strore=True, force_save=True,
+                                      string="size", related='product_id.product_size_id')
+    product_zone_id = fields.Many2one('product.zone', strore=True, force_save=True,
+                                      string="Product Zone", related='product_id.product_zone_id')
+    product_shelf_id = fields.Many2one('product.shelf', strore=True, force_save=True,
+                                       string="Product Shelf", related='product_id.product_shelf_id')
